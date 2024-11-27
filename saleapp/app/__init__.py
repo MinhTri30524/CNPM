b0ff57e8667d8e68fc3fb82b311df7dffe1bec87
duyan77 <fklt0961@gmail.com>
2024-11-27 15:06:46 +0700

    an commit

--- saleapp/app/__init__.py
+++ saleapp/app/__init__.py
@@ -1,12 +1,14 @@
-from flask import Flask
 from urllib.parse import quote
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+
 import cloudinary
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "KJGHJG^&*%&*^T&*(IGFG%ERFTGHCFHGF^&**&TYIU"
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:%s@localhost/saledb?charset=utf8mb4" % quote('Admin@123')
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+		"mysql+pymysql://root:%s@localhost/saledb?charset=utf8mb4" % quote('admin123'))
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 app.config["PAGE_SIZE"] = 8
 
@@ -14,8 +16,10 @@ db = SQLAlchemy(app)
 login = LoginManager(app)
 
 cloudinary.config(
-    cloud_name="dxxwcby8l",
-    api_key="448651448423589",
-    api_secret="ftGud0r1TTqp0CGp5tjwNmkAm-A",  # Click 'View API Keys' above to copy your API secret
-    secure=True
+	cloud_name="dxxwcby8l",
+	api_key="448651448423589",
+	api_secret="ftGud0r1TTqp0CGp5tjwNmkAm-A",  # Click 'View API Keys' above to copy your API secret
+	secure=True
 )
+
+
